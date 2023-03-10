f2c1d3c387ed2c104609ee51bfacdf17d255d45d
Jason Field <Jason@avon-lea.co.uk>
2023-03-10 11:35:52 +0000

    fix: Resolve CI issues, make concurrent (#1)
    Makes concurrent and fixes ci issues
    
    Cannot mark as "done" but can at least make it easier to delete/filter
    
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- manager/manager.go
+++ manager/manager.go
@@ -15,7 +15,6 @@ import (
 )
 
 var ctx = context.Background()
-var mutex sync.Mutex
 
 type Manager struct {
 	client *github.Client
@@ -44,9 +43,9 @@ func (m *Manager) Handle() {
 	m.log.Infof("Found %d notifications", len(nots))
 	nots = m.ValidateOrg(nots)
 	m.log.Infof("Found %d notifications for org %s", len(nots), m.cfg.OrgName)
-	nots = m.ValidateRepo(nots)
+	nots, _ = m.ValidateRepoConcurrent(nots)
 	m.log.Infof("Found %d notifications for repo with appropriate states %s", len(nots), m.cfg.State)
-	m.MarkAsRead(nots)
+	m.MarkAsReadConcurrent(nots)
 }
 
 func (m *Manager) ListNotifications() ([]*github.Notification, error) {
@@ -82,77 +81,63 @@ func (m *Manager) ValidateOrg(n []*github.Notification) []*github.Notification {
 	return response
 }
 
-type cache struct {
-	n []*github.Notification
+func (m *Manager) MarkAsReadConcurrent(n []*github.Notification) {
+	var wg sync.WaitGroup
+	for _, v := range n {
+		wg.Add(1)
+		go func(v *github.Notification) {
+			defer wg.Done()
+			if m.cfg.DryRun {
+				m.log.Infof("Would mark %s\t for repo %s \t id: %s as read", v.GetSubject().GetTitle(), v.GetRepository().GetFullName(), v.GetID())
+				return
+			}
+			_, err := m.client.Activity.MarkThreadRead(ctx, v.GetID())
+			if err != nil {
+				m.log.Errorf("Error marking thread as read %s", err.Error())
+			}
+			_, err = m.client.Activity.DeleteThreadSubscription(ctx, v.GetID())
+			if err != nil {
+				m.log.Errorf("Error deleting thread subscription %s", err.Error())
+			}
+
+		}(v)
+	}
+	wg.Wait()
 }
 
-func (m *Manager) ValidateRepo(n []*github.Notification) []*github.Notification {
+func (m *Manager) ValidateRepoConcurrent(n []*github.Notification) ([]*github.Notification, []*github.Notification) {
 	var response []*github.Notification
-	total := len(n)
-	i := 1
+	var NotMatched []*github.Notification
+	var wg sync.WaitGroup
 	for _, v := range n {
-		m.log.Infof("Processing %d/%d", i, total)
-		i++
-		if v.GetSubject().GetType() == "PullRequest" {
-			pr, _, err := m.client.PullRequests.Get(ctx, v.GetRepository().GetOwner().GetLogin(), v.GetRepository().GetName(), getId(v.GetSubject().GetURL()))
-			if err != nil {
-				m.log.Errorf("Error getting pr %s for %s", err.Error(), v.GetSubject().GetURL())
-			}
-			if slices.Contains(m.cfg.GetState(), pr.GetState()) {
-				response = append(response, v)
-			}
-		}
-		if v.GetSubject().GetType() == "Issue" {
-			issue, _, err := m.client.Issues.Get(ctx, v.GetRepository().GetOwner().GetLogin(), v.GetRepository().GetName(), getId(v.GetSubject().GetURL()))
-			if err != nil {
-				m.log.Errorf("Error getting issue %s for %s", err.Error(), v.GetSubject().GetURL())
+		wg.Add(1)
+		go func(v *github.Notification) {
+			defer wg.Done()
+			if v.GetSubject().GetType() == "PullRequest" {
+				pr, _, err := m.client.PullRequests.Get(ctx, v.GetRepository().GetOwner().GetLogin(), v.GetRepository().GetName(), getId(v.GetSubject().GetURL()))
+				if err != nil {
+					m.log.Errorf("Error getting pr %s for %s", err.Error(), v.GetSubject().GetURL())
+				}
+				if slices.Contains(m.cfg.GetState(), pr.GetState()) {
+					response = append(response, v)
+				}
+			} else if v.GetSubject().GetType() == "Issue" {
+				issue, _, err := m.client.Issues.Get(ctx, v.GetRepository().GetOwner().GetLogin(), v.GetRepository().GetName(), getId(v.GetSubject().GetURL()))
+				if err != nil {
+					m.log.Errorf("Error getting issue %s for %s", err.Error(), v.GetSubject().GetURL())
 
+				}
+				if slices.Contains(m.cfg.GetState(), issue.GetState()) {
+					response = append(response, v)
+				}
+			} else {
+				NotMatched = append(NotMatched, v)
 			}
-			if slices.Contains(m.cfg.GetState(), issue.GetState()) {
-				response = append(response, v)
-			}
-		}
+		}(v)
 	}
-	return response
+	wg.Wait()
+	return response, NotMatched
 }
-
-func (m *Manager) MarkAsRead(n []*github.Notification) {
-
-	for _, v := range n {
-		if m.cfg.DryRun {
-			m.log.Infof("Would mark %s\t for repo %s \t id: %s as read", v.GetSubject().GetTitle(), v.GetRepository().GetFullName(), v.GetID())
-			continue
-		}
-		_, err := m.client.Activity.MarkThreadRead(ctx, v.GetID())
-		if err != nil {
-			m.log.Errorf("Error marking as read %s", err.Error())
-		}
-	}
-}
-
-//func main() {
-//	client := NewGithubClientPAT(ctx, os.Getenv("GITHUB_TOKEN"))
-//	opts := &github.NotificationListOptions{All: false}
-//	nots, _, err := client.Activity.ListNotifications(ctx, opts)
-//	if err != nil {
-//		panic(err)
-//	}
-//	for _, n := range nots {
-//		fmt.Printf("notification: %s,\t for repo %s,\t due to %s \tand is currently %s\t url: %s\n", n.GetSubject().GetTitle(), n.GetRepository().GetFullName(), n.GetReason(), n.GetSubject().GetTitle(), n.GetSubject().GetURL())
-//		if n.GetSubject().GetType() == "PullRequest" && n.GetRepository().GetOwner().GetLogin() == "sous-chefs" {
-//			pr, _, err := client.PullRequests.Get(ctx, n.GetRepository().GetOwner().GetLogin(), n.GetRepository().GetName(), getId(n.GetSubject().GetURL()))
-//			if err != nil {
-//				panic(err)
-//			}
-//			if pr.GetState() == "closed" || pr.GetState() == "merged" {
-//				fmt.Printf("PR is in state %s\t and is being marked as read\n", pr.GetState())
-//				client.Activity.MarkThreadRead(ctx, n.GetID())
-//				continue
-//			}
-//			fmt.Printf("PR is in state %s\t and has %d comments\n", pr.GetState(), pr.GetComments())
-//		}
-//	}
-//}
 
 func getId(url string) int {
 	last := url[strings.LastIndex(url, "/")+1:]
